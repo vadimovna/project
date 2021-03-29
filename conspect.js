37bb4f1cb1a13d8a0eee8c5b72e9e9ba6cc286d2
Evelina <evi.tremaova@mail.ru>
2021-03-29 17:40:37 +0300

    first commit

---
+++ conspect.js
@@ -0,0 +1,102 @@
+"use strict";
+
+/* let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj); */
+
+/* let number = 4.6; 
+
+console.log(4/0);
+console.log('string' * 9);
+
+const persone = '5';
+
+const bool = true;
+
+console.log(something); null
+
+let und;
+console.log(und); undefined
+
+const obj = {   объекты
+    name: 'John',
+    age: 25,
+    isMaried: false
+};
+
+// console.log(obj.name);
+console.log(obj ['name']); 
+
+
+                0           1        2        3      4    5
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []]; массивы
+console.log(arr[1]); */
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+//const answer = +prompt('Вам есть 18?', '18'); инф от пользователя приходит в виде строки, 
+//но если поставим + перед prompt, то тип данных изменится на числовой
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваша фамилия?', '');
+//answers[2] = prompt('Сколько вам лет?', '');
+
+//console.log(typeof(answers));
+//console.log(typeof(null));
+
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`)
+
+//const user = 'Ivan';
+
+//alert(`Привет, ${user}`)
+
+console.log('arr' + ' - object');
+console.log(4 + '5');
+
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;  
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 === 8); // == сравнение без типа данных, === строгое сравнение по типу 
+
+&& //оператор "и" работает с двумя правдивыми выражениями
+|| //оператор "или" хотя бы одно выражение должно быть правдиво
+! //оператор отрицания, ложь в правду и наоборот
+!= //не равен
+
+const isCheked = true,
+      isClose = false;
+
+console.log(isCheked || !isClose);
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+/* let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj); */
+
+/* let number = 4.6; 
+
+console.log(4/0);
+console.log('string' * 9);
+
+const persone = '5';
+
+const bool = true;
+
+console.log(something); null
+
+let und;
+console.log(und); undefined
+
+const obj = {   объекты
+    name: 'John',
+    age: 25,
+    isMaried: false
+};
+
+// console.log(obj.name);
+console.log(obj ['name']); 
+
+
+                0           1        2        3      4    5
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []]; массивы
+console.log(arr[1]); */
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+//const answer = +prompt('Вам есть 18?', '18'); инф от пользователя приходит в виде строки, 
+//но если поставим + перед prompt, то тип данных изменится на числовой
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваша фамилия?', '');
+//answers[2] = prompt('Сколько вам лет?', '');
+
+//console.log(typeof(answers));
+//console.log(typeof(null));
+
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`)
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`)
