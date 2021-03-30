c658d7e899de42546d79ea769b6b830c40e7db96
Evelina <evi.tremaova@mail.ru>
2021-03-30 11:45:53 +0300

    add movie app task 1

--- script.js
+++ script.js
@@ -1,70 +1,23 @@
 "use strict";
 
-/* let number = 5;
-const leftBorderWidth = 1;
-
-number = 10;
-console.log(number);
-
-const obj = {
-    a: 50
-};
-
-obj.a = 10;
-
-console.log(obj); */
-
-/* let number = 4.6; 
-
-console.log(4/0);
-console.log('string' * 9);
-
-const persone = '5';
-
-const bool = true;
-
-console.log(something); null
-
-let und;
-console.log(und); undefined
-
-const obj = {   объекты
-    name: 'John',
-    age: 25,
-    isMaried: false
-};
-
-// console.log(obj.name);
-console.log(obj ['name']); 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); 
 
 
-                0           1        2        3      4    5
-let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []]; массивы
-console.log(arr[1]); */
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}; 
 
-// alert('Hello');
+const a = prompt('Один из  последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из  последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', ''); 
 
-// const result = confirm('Are you here?');
-// console.log(result);
+personalMovieDB.movies[a] = b;   
+personalMovieDB.movies[c] = d; 
 
-//const answer = +prompt('Вам есть 18?', '18'); инф от пользователя приходит в виде строки, 
-//но если поставим + перед prompt, то тип данных изменится на числовой
-//console.log(answer + 5);
+console.log(personalMovieDB);
 
-//const answers = [];
-
-//answers[0] = prompt('Как ваше имя?', '');
-//answers[1] = prompt('Как ваша фамилия?', '');
-//answers[2] = prompt('Сколько вам лет?', '');
-
-//console.log(typeof(answers));
-//console.log(typeof(null));
-
-
-const category = 'toys';
-
-console.log(`https://someurl.com/${category}/5`)
-
-const user = 'Ivan';
-
-alert(`Привет, ${user}`)
